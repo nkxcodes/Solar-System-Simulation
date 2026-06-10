4d4eb763ab4163748d2b962390fbddf05e34d1c0
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-10 14:19:05 +0530

    Add Planet class to planets.py for enhanced planet data representation, including attributes like diameter, distance from sun, main color, moons, and orbital speed.

--- planets.py
+++ planets.py
@@ -2,3 +2,11 @@
 
 solar_system_planets = ['Earth', 'Moon', 'Venus', 'Jupiter', 'Saturn', 'Mars', 'Neptune', 'Uranus', 'Pluto', 'Sun']
 exo_planets = ['100-viger-2', '908-helux-45']
+
+class Planet:
+    def __init__(self, diameter, distance_from_sun, main_color, moons, orbital_speed):
+        self.diameter = diameter
+        self.distance_from_sun = distance_from_sun
+        self.main_color = main_color
+        self.moons = moons
+        self.orbital_speed = orbital_speed
