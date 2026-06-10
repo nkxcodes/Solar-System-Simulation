beca45b561b965f102aff94ab4f29c3ded620a53
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-10 14:04:16 +0530

    Implement basic Pygame setup in main.py for Solar System Simulation, including window initialization and event handling.

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 # Solar System Simulation
 # Main.py
 
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Solar System Simulation")
+
 def main():
     print("Welcome to the Solar System Simulation!")
     print("This is a simulation of the solar system.")
     print("- Nitesh Kumar - 2026")
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False;
+        
+        pygame.display.update();
+
 
 if __name__ == "__main__":
     main()
