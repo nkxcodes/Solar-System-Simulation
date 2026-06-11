85280662e2bda92ff64430397d3c517ff0e5f889
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-11 20:31:12 +0530

    Add planet images rendering to main.py and update sun asset

--- main.py
+++ main.py
@@ -19,6 +19,16 @@ def main():
     while running:
         for event in pygame.event.get():
             screen.blit(BACKGROUND_IMG, (0, 0))
+            screen.blit(EARTH_IMG, (50, 150))
+            screen.blit(JUPITER_IMG, (200, 150))
+            screen.blit(MARS_IMG, (350, 150))
+            screen.blit(MERCURY_IMG, (500, 150))
+            screen.blit(NEPTUNE_IMG, (650, 150))
+            screen.blit(SATURN_IMG, (800, 150))
+            screen.blit(URANUS_IMG, (950, 150))
+            screen.blit(VENUS_IMG, (50, 350))
+            screen.blit(SUN_IMG, (200, 350))
+            screen.blit 
             if event.type == pygame.QUIT:
                 running = False;
         pygame.display.update();
