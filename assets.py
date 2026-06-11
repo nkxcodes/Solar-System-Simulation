71f6e7e42e8728154c323f196524fe7adabbdb56
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-11 15:47:42 +0530

    Add assets for solar system simulation, including planet images and a new background image. Removed obsolete Neptune asset.

---
+++ assets.py
@@ -0,0 +1,50 @@
+import pygame
+
+EARTH_IMG = pygame.transform.scale(
+    pygame.image.load("assets/earth-planet.png").convert_alpha(),
+    (150, 150)
+)
+
+JUPITER_IMG = pygame.transform.scale(
+    pygame.image.load("assets/jupiter-planet.png").convert_alpha(),
+    (150, 150)
+)
+
+MARS_IMG = pygame.transform.scale(
+    pygame.image.load("assets/mars-planet.png").convert_alpha(),
+    (150, 150)
+)
+
+MERCURY_IMG = pygame.transform.scale(
+    pygame.image.load("assets/mercury-planet.png").convert_alpha(),
+    (150, 150)
+)
+
+NEPTUNE_IMG = pygame.transform.scale(
+    pygame.image.load("assets/neptune-planet.png").convert_alpha(),
+    (150, 150)
+)
+
+URANUS_IMG = pygame.transform.scale(
+    pygame.image.load("assets/uranus-planet.png").convert_alpha(),
+    (150, 150)
+)
+
+VENUS_IMG = pygame.transform.scale(
+    pygame.image.load("assets/venus-planet.png").convert_alpha(),
+    (150, 150)
+)
+
+SUN_IMG = pygame.transform.scale(
+    pygame.image.load("assets/sun.png").convert_alpha(),
+    (150, 150)
+)
+
+SATURN_IMG = pygame.transform.scale(
+    pygame.image.load("assets/saturn-planet.png").convert_alpha(),
+    (150, 150)
+)
+
+BACKGROUND_IMG = pygame.transfrom.scale(
+    pygame.image.load("assets/stars-at-night-sky.png")
+)
--- main.py
+++ main.py
@@ -18,7 +18,6 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False;
-        
         pygame.display.update();
 
 
