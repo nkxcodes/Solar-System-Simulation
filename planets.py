0974db83e1e71b7174ca3d00b949ba9b40482f55
Nitesh <niteshkjaiswal108@gmail.com>
2026-06-17 17:29:26 +0530

    Refactor Planet class constructor in planets.py to remove main_color and moons parameters, simplifying object instantiation for improved clarity in Solar System Simulation.

--- planets.py
+++ planets.py
@@ -4,9 +4,7 @@ solar_system_planets = ['Earth', 'Moon', 'Venus', 'Jupiter', 'Saturn', 'Mars', '
 exo_planets = ['Kepler-452b', 'Proxima Centauri b', 'HD 209458 b', 'WASP-12b', 'TRAPPIST-1e']
 
 class Planet:
-    def __init__(self, diameter, distance_from_sun, main_color, moons, orbital_speed, current_x_position, current_y_position):
+    def __init__(self, diameter, distance_from_sun, orbital_speed, current_x_position, current_y_position):
         self.diameter = diameter
         self.distance_from_sun = distance_from_sun
-        self.main_color = main_color
-        self.moons = moons
         self.orbital_speed = orbital_speed
